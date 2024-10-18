8b1ea39648e3de4ec5b4b09b7925bcf541960038
vanci <vancura.david@gmail.com>
2024-10-18 15:03:30 +0200

    pridani modalniho okna a uprava stylu

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
-function showForm() {
-	document.getElementById("myForm").style.display = "block";
-}
+// Získání modálního okna a tlačítek
+let modal = document.getElementById("myModal");
+let btn = document.getElementById("openModal");
+let span = document.getElementById("closeModal");
+
+// Otevření modálního okna při kliknutí na tlačítko
+btn.onclick = function () {
+	modal.style.display = "block";
+};
+
+// Zavření modálního okna při kliknutí na "x"
+span.onclick = function () {
+	modal.style.display = "none";
+};
+
+// Zavření modálního okna při kliknutí mimo okno
+window.onclick = function (event) {
+	if (event.target == modal) {
+		modal.style.display = "none";
+	}
+};
